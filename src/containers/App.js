2fc3c59b953d0c1e8b13f7ac460cbeeb09c798bb
dsdunn <dsdunnmusic@gmail.com>
2018-07-19 13:57:39 -0600

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import {rootReducer } from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+const store = createStore(rootReducer, devTools);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const router = (
+  <Provider store= {store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
+registerServiceWorker();
