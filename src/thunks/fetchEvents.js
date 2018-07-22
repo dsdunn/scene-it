c349173a578282087fdd2fc659a42f7efa04ba61
dsdunn <dsdunn@Davids-iMac.local>
2018-07-22 15:01:52 -0600

    Write dataCleaner

--- src/thunks/fetchEvents.js
+++ src/thunks/fetchEvents.js
@@ -1,17 +1,19 @@
 import { isLoading, hasErrored, eventsFetchSuccess } from '../actions';
+import { dataCleaner } from '../helper';
 
 export const fetchEvents = (url) => {
   return async (dispatch) => {
     try {
       dispatch(isLoading(true));
-      const response = await fetch(url);
+      const response = await fetch(url,{mode:"cors"});
       if (!response.ok) {
         throw Error(response.statusText);
       }
       dispatch(isLoading(false));
       const result = await response.json();
-      dispatch(eventsFetchSuccess(result));
-      console.log(result);
+      const cleanData = dataCleaner(result.events.event);
+      dispatch(eventsFetchSuccess(cleanData));
+      console.log(result.events.event);
     } catch (error) {
       dispatch(hasErrored(true));
     }
