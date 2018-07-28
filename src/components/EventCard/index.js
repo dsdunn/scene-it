f2e8c256b133f9ddb48047d486dd9f3e86d57a72
dsdunn <dsdunnmusic@gmail.com>
2018-07-28 11:51:32 -0600

    Add start Time to eventCard

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -7,6 +7,7 @@ export const EventCard = ({event, selectEvent}) => {
   return (
     <div className="event-card" id={event.id} onClick={handleClick}>
       <h4 className="card-title">{event.title}</h4>
+      <h5 className="date">{event.startTime}</h5>
       <p className="card-venue">{event.venueName}</p>
     </div>
     )
