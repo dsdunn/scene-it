04bac87338f40ab34b5f2f0aa04400c2097ba3dc
dsdunn <dsdunnmusic@gmail.com>
2018-07-30 17:20:10 -0600

    Fix text alignment in details

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -36,23 +36,25 @@ export const Details = ({selectedEvent}) => {
           <div className='details-header-left'>
             <h1 className="details-title">{title}</h1>
             <p className="details-genre">{getBio()}</p>
+            <Moment 
+              className="details-start-time"
+              date={startTime}
+              format="dddd, MMMM Do YYYY, hA"
+            /> 
+            <p className="event-url">
+              <a target="_blank" href={eventUrl}>get tickets</a>
+            </p>
           </div>
-          <h2 className="details-venue">
-            <a target="_blank" href={venueUrl}>{venueName}</a>
-          </h2>
-          <Moment 
-            className="details-start-time"
-            date={startTime}
-            format="dddd, MMMM Do YYYY, hA"
-          /> 
-          <div className="address">
-            <p>{address}</p>
-            <p>{region}, {postalCode}</p>
+          <div className="details-header-right">
+            <h2 className="details-venue">
+              <a target="_blank" href={venueUrl}>{venueName}</a>
+            </h2>
+            <div className="address">
+              <p>{address}</p>
+              <p>{region}, {postalCode}</p>
+            </div>
           </div>
         </div>  
-        <p className="event-url">
-          <a target="_blank" href={eventUrl}>get tickets</a>
-        </p>
         <p dangerouslySetInnerHTML={{__html: description}} 
           className="details-description"></p>
       </div>  
